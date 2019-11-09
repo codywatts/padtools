c74e56f578a5dce25323372d0302a098953989d2
nachoapps <nachoappsinc@gmail.com>
2019-11-09 11:00:42 -0500

    Bump version

--- padtools/__init__.py
+++ padtools/__init__.py
@@ -1,7 +1,7 @@
 from .regions import Regions as regions
 from .servers import Servers as servers
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __title__ = "padtools"
 __description__ = """A package for the popular iOS & Android game "Puzzle & Dragons" (PAD)"""
